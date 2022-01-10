56c08a9f514d92f6b30c8904d2c18ee578a9eed5
Hetu Patel <hetu200211@gmail.com>
2022-01-10 18:35:51 +0530

    side bar

--- src/container/Navbar/Nav.js
+++ src/container/Navbar/Nav.js
@@ -1,35 +1,82 @@
-import React from 'react'
-import '../style/Nav.scss'
+import React from "react";
+import "../style/Nav.scss";
 
 let toggle = 0;
+let drop = 0;
 function Nav() {
-    return (
-        <nav class="top-nav">
-                <div className='left-nav' id='left-nav'></div>
-                <div className='right-nav' id='right-nav'>
-                <button className='nav-toggle' onClick={()=>{
-                    if(toggle==0){
-                    document.getElementById('left-nav').style.width = "170px";
-                    document.getElementById('right-nav').style.width = "calc(100% - 170px)";
-                    document.getElementById('sidebar').style.width = "170px";
-                    document.getElementById('main-home-page').style.width = "calc(100% - 170px)";
-                    toggle = 1;
-                    }
-                    else{
-                        document.getElementById('left-nav').style.width = "70px";
-                    document.getElementById('right-nav').style.width = "calc(100% - 70px)";
-                    document.getElementById('sidebar').style.width = "70px";
-                    document.getElementById('main-home-page').style.width = "calc(100% - 70px)"
-                    toggle = 0;
-                    }
-                }}>
-                     <span></span>   
-                     <span></span>
-                     <span></span>
-                </button>
-                </div>
-        </nav>
-    )
+  return (
+    <nav class="top-nav">
+      <div className="left-nav" id="left-nav">
+          <div>
+          <img src="https://cdn.iconscout.com/icon/free/png-256/notepad-2642816-2192663.png"/>
+          </div>
+      </div>
+      <div className="right-nav" id="right-nav">
+        <button
+          className="nav-toggle"
+          onClick={() => {
+            if (toggle == 0) {
+              document.getElementById("left-nav").style.width = "170px";
+              document.getElementById("left-nav").style.justifyContent = "center";
+              document.getElementById("right-nav").style.width =
+                "calc(100% - 170px)";
+              document.getElementById("sidebar").style.width = "170px";
+              document.getElementById("main-home-page").style.width =
+                "calc(100% - 170px)";
+              toggle = 1;
+            } else {
+              document.getElementById("left-nav").style.width = "70px";
+              document.getElementById("right-nav").style.width =
+                "calc(100% - 70px)";
+                document.getElementById("left-nav").style.justifyContent = "flex-end";
+              document.getElementById("sidebar").style.width = "70px";
+              document.getElementById("main-home-page").style.width =
+                "calc(100% - 70px)";
+              toggle = 0;
+            }
+          }}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <div className="nav-search">
+          <input type="text" placeholder="search here..." />
+        </div>
+        <div className="nev-right-right">
+          <div className="nev-right-button">
+            <button><i class="fas fa-plus"></i> create room</button>
+          </div>
+          <div className="nev-right-profile">
+            <div className="nav-profile" onClick={()=>{
+                if(drop==0){
+                    document.getElementById('nav-profile-drop').style.top = "90%";
+                    drop = 1;
+                }
+                else{
+                    document.getElementById('nav-profile-drop').style.top = "-150px";
+                    drop = 0;
+                }
+            }}>
+              <img src="https://cdn.iconscout.com/icon/premium/png-256-thumb/profile-1506810-1278719.png"/>
+              <h4>User</h4>
+              <i class="fas fa-sort-down"></i>
+            </div>
+            <div className="nav-profile-drop" id="nav-profile-drop">
+              <div>
+                <i class="fas fa-user-alt"></i>
+                <h4>Profile</h4>
+              </div>
+              <div>
+                <i class="fas fa-sign-out-alt"></i>
+                <h4>Log-Out</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Nav
+export default Nav;
--- src/container/Navbar/Sidebar.js
+++ src/container/Navbar/Sidebar.js
@@ -1,11 +1,55 @@
-import React from 'react'
-import '../style/Nav.scss'
+import React from "react";
+import "../style/Nav.scss";
+
+const sidebarLi = [
+  {
+    name: "Home",
+    icon: "fas fa-home",
+  },
+  {
+    name : "Friends",
+    icon :"fas fa-user-friends",
+  },
+  {
+    name: "Rooms",
+    icon: "far fa-sticky-note",
+  },
+  {
+      name : "About",
+      icon : "fas fa-snowman",
+  },
+  {
+      name : "Log-Out",
+      icon : "fas fa-sign-out-alt"
+  },
+  
+];
 function Sidebar() {
-    return (
-        <div className='sidebar' id='sidebar'>
-            
+  return (
+    <div className="sidebar" id="sidebar">
+      <div className="side-profile">
+        <div className="profile-round">
+          <img src="https://cdn4.vectorstock.com/i/thumb-large/77/83/cute-young-man-with-glasses-avatar-cartoon-style-vector-36327783.jpg" />
+          <div className="prof-round-bottom"></div>
         </div>
-    )
+        <div className="side-prof-name">
+          <h2>Login</h2>
+        </div>
+      </div>
+      {sidebarLi.map((res) => {
+        return (
+          <div className="sidebar-li">
+            <div className="sidebar-li-ico">
+              <i class={res.icon}></i>
+            </div>
+            <div className="sidebar-li-h2">
+              <h2>{res.name}</h2>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
